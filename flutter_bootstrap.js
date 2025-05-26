a3adba251d84930811ed353f46d57f68db641f57
bidryde-bharathkumar-admin <bharath.kumar@bidryde.in>
2025-05-26 17:00:29 +0530

    assets

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2680322836"
+    serviceWorkerVersion: "2131141288"
   }
 });
